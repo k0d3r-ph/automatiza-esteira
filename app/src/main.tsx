ed0fba807b93834e3216219e4254edca65cf6dfd
Pedro Henrique <pedrohenriqueoliveira29@hotmail.com>
2026-05-20 17:22:20 -0300

    Importação de um navegador para mais pages

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,20 +4,23 @@ import { Toaster } from "sonner";
 import "sonner/dist/styles.css";
 import "./index.css";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <Toaster
-      richColors
-      toastOptions={{
-        style: {
-          fontSize: "16px",
-          padding: "18px 20px",
-          minWidth: "320px",
-        },
-      }}
-      position="top-right"
-    />
+    <BrowserRouter>
+      <App />
+      <Toaster
+        richColors
+        toastOptions={{
+          style: {
+            fontSize: "16px",
+            padding: "18px 20px",
+            minWidth: "320px",
+          },
+        }}
+        position="top-right"
+      />
+    </BrowserRouter>
   </StrictMode>,
 );
